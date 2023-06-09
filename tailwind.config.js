0aab0b8c9f889fb408ce1d32d91caea2d3fbd601
IvanAlvear <pedrivavfy@gmail.com>
2023-06-08 19:10:41 -0500

    ajustar color del proyecto

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,8 @@
 import defaultTheme from 'tailwindcss/defaultTheme';
 import forms from '@tailwindcss/forms';
 
+const colors = require('tailwindcss/colors');
+
 /** @type {import('tailwindcss').Config} */
 export default {
     content: [
@@ -8,14 +10,23 @@ export default {
         './storage/framework/views/*.php',
         './resources/views/**/*.blade.php',
     ],
-
+    darkMode: 'class',
     theme: {
         extend: {
             fontFamily: {
                 sans: ['Figtree', ...defaultTheme.fontFamily.sans],
             },
+            colors: { 
+                danger: colors.rose,
+                primary: colors.red,
+                success: colors.green,
+                warning: colors.yellow,
+            }, 
         },
     },
 
-    plugins: [forms],
+    plugins: [
+        require('@tailwindcss/forms'), 
+        require('@tailwindcss/typography'), 
+    ],
 };
